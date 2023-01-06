3626462ef9265ef4185d35fd7c7a0e496d35e365
Loy <loytakian@gmail.com>
2023-01-05 22:21:22 -0300

    changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/runner/Python/venv"
+set -gx VIRTUAL_ENV "/home/loy/projects/Telegram-Bot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
